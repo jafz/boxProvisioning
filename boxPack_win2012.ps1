7e593fedd945bbc8508b0ccbabd71aba49d20a15
jafz <karotte@gmail.com>
2017-06-29 18:28:49 +0200

    installation packages for win2012

---
+++ boxPack_win2012.ps1
@@ -0,0 +1,23 @@
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+# Basic setup
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Enable-RemoteDesktop
+Disable-InternetExplorerESC
+Disable-UAC
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Update Windows and reboot if necessary
+Install-WindowsUpdate -AcceptEula
+if (Test-PendingReboot) { Invoke-Reboot }
+
+cinstm baretail
+
+cinstm notepadpluslus.install
+cinstm googlechrome
+
+cinstm 7zip

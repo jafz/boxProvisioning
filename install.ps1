4ff79ce55489071b85f75ef33422adcf833a452f
jafz <karotte@gmail.com>
2017-06-29 17:50:28 +0200

    more

--- install.ps1
+++ install.ps1
@@ -1,8 +1,9 @@
 
-Update-ExecutionPolicy -Policy Unrestricted
 . { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 Enable-RemoteDesktop
 netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
 
+Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
 
-Install-WindowsUpdate -AcceptEula
+(new-object net.webclient).DownloadFile('https://github.com/jafz/boxProvisioning/raw/master/boxPack.ps1','c:\boxPack.ps1')
+Install-BoxstarterPackage -PackageName c:\boxPack.ps1

56bcd1dbdd35b80f27de9ce03c9287289d506cdd
Jakob Ziegler <jakob.ziegler@skilja.com>
2017-06-30 18:01:34 +0200

    some more packages to install

--- boxStarterVarious.ps1
+++ boxStarterVarious.ps1
@@ -10,11 +10,13 @@ cinst baretail
 #Other dev tools
 cinst fiddler4
 cinst notepadplusplus.install
+cinst sublimetext3
 cinst notepad2
 cinst beyondcompare
 cinst sysinternals
 cinst NugetPackageExplorer
 cinst windbg
+cinst postman
 
 #Browsers
 cinst googlechrome
@@ -24,3 +26,11 @@ cinst firefox
 cinst 7zip
 cinst nitroreader.install
 cinst javaruntime
+
+cinst bitvise-ssh-client
+cinst listary
+cinst docker-for-windows
+cinst greenshot
+
+# brutal-dev .net assembly strong namer missing
+# samuel attard google music player

f5c46396bb6da3cd93e425057fd5796f68c2095b
Jakob Ziegler <jakob.ziegler@skilja.com>
2017-06-30 09:54:27 +0200

    more tries to get boxstarter to run automatically

--- install.ps1
+++ install.ps1
@@ -5,5 +5,6 @@ netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 
 
 Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
 
-(new-object net.webclient).DownloadFile('https://github.com/jafz/boxProvisioning/raw/master/boxPack.ps1','c:\boxPack.ps1')
-Install-BoxstarterPackage -PackageName c:\boxPack.ps1
+#(new-object net.webclient).DownloadFile('https://github.com/jafz/boxProvisioning/raw/master/boxPack.ps1','c:\boxPack.ps1')
+$cred=Get-Credential
+Install-BoxstarterPackage -PackageName https://github.com/jafz/boxProvisioning/raw/master/boxPack.ps1 -Credential $cred

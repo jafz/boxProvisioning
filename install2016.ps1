7a27ea359dc6c4f9520f9669f1e676e1221e37ae
Jakob Ziegler <jakob.ziegler@skilja.com>
2017-07-26 11:52:26 +0200

    iwr with -usebasicparsing (windows without IE errors otherwise)

--- install2016.ps1
+++ install2016.ps1
@@ -1,7 +1,7 @@
 # to run, execute this in an elevated ps cmd prompt:
 # set-executionpolicy unrestricted -force; (new-object net.webclient).DownloadFile('https://github.com/jafz/boxProvisioning/raw/master/install2016.ps1','c:\install2016.ps1'); c:\install2016.ps1
 # iwr == Invoke-WebRequest, iex = Invoke-Expression
-# set-executionpolicy unrestricted -force; $script = iwr -URI https://github.com/jafz/boxProvisioning/raw/master/install2016.ps1; iex $($script.Content)
+# set-executionpolicy unrestricted -force; $script = iwr -URI https://github.com/jafz/boxProvisioning/raw/master/install2016.ps1 -UseBasicParsing; iex $($script.Content)
 
 . { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 Enable-RemoteDesktop

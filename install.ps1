313a89e187b390d50ed08798c6da85d0e266ca07
Jakob Ziegler <jakob.ziegler@skilja.com>
2017-07-26 11:42:25 +0200

    update iwr calls

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 # set-executionpolicy unrestricted -force; (new-object net.webclient).DownloadFile('https://github.com/jafz/boxProvisioning/raw/master/install.ps1','c:\install.ps1'); c:\install.ps1
 # iwr == Invoke-WebRequest, iex = Invoke-Expression
-# set-executionpolicy unrestricted -force; $script = iwr(https://github.com/jafz/boxProvisioning/raw/master/install.ps1); iex $($script.Content)
+# set-executionpolicy unrestricted -force; $script = iwr -URI https://github.com/jafz/boxProvisioning/raw/master/install.ps1; iex $($script.Content)
 
 . { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 Enable-RemoteDesktop

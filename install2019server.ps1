77c9ff90349351b1ac0e2965ffb9b210928087f6
Jakob <karotte@gmail.com>
2020-11-09 12:12:01 +0100

    win2019 install scripts

---
+++ install2019server.ps1
@@ -0,0 +1,14 @@
+# set-executionpolicy unrestricted -force; (new-object net.webclient).DownloadFile('https://github.com/jafz/boxProvisioning/raw/master/install.ps1','c:\install.ps1'); c:\install.ps1
+# iwr == Invoke-WebRequest, iex = Invoke-Expression
+# set-executionpolicy unrestricted -force; $script = iwr -URI https://github.com/jafz/boxProvisioning/raw/master/install.ps1; iex $($script.Content)
+
+. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+Enable-RemoteDesktop
+Disable-InternetExplorerESC
+Disable-UAC
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+
+Enable-PSRemoting -Force
+cinst googlechrome -y
+cinst notepadplusplus -y
+cinst baretail -y
